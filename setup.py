2aef10b4a9691292618c1faf6b662883088862d2
zach <zachkdavis00@gmail.com>
2023-03-25 20:56:26 -0400

    getting ready for pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(name='ModelFitter',
 version='0.1',
-description='Testing installation of Package',
-url='#',
-author='auth',
-author_email='',
+# description='Testing installation of Package',
+# url='#',
+author='Zach Davis',
+author_email='zachkdavis00@gmail.com',
 license='MIT',
 packages=['ModelFitter'],
+install_requires=[
+  "matplotlib>=3.7.1",
+  "numpy==1.24.2",
+  "easierplotlib>=0.2",
+],
 zip_safe=False)
